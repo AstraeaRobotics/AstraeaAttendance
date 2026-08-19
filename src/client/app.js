f199a518789a085fb214d4e6580390ef1c5528c9
Sohum Bhardwaj <sohumb999@gmail.com>
2026-08-19 12:00:19 -0400

    got rid of the video stuff

--- src/client/app.js
+++ src/client/app.js
@@ -1,26 +1,7 @@
-import { BarcodeFormat } from "@zxing/library";
-import { BrowserMultiFormatReader } from "@zxing/browser";
-
-const video = document.querySelector("#webcam");
 const submit = document.querySelector("#submit-id");
 const idEntry = document.querySelector("#student-id-entry");
 const recentCheckins = document.querySelector("#recent-checkins");
 const manualEntryStatus = document.querySelector("#status-manual-entry")
-
-const reader = new BrowserMultiFormatReader();
-reader.possibleFormats = [BarcodeFormat.AZTEC];
-
-async function startCamera() {
-    const stream = await navigator.mediaDevices.getUserMedia({
-        video: {
-            width: 400,
-            height: 200
-        }
-    });
-    video.srcObject = stream;
-} startCamera();
-
-
 
 function addCheckin(id, type) {
     const entry = document.createElement("li");
@@ -42,13 +23,3 @@ submit.addEventListener("click", () => {
         manualEntryStatus.textContent = "INVALID ID: please try again!"
     }
 })
-
-reader.decodeFromVideoDevice(
-    undefined,
-    video,
-    (result, error) => {
-        if (result) {
-            addCheckin(result.getText(), "Scanned");
-        }
-    }
-);
